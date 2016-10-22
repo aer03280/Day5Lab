f155ca323bbb676da9c918e0e3bdd872e9d5d57b
Ashley Ross <ashley.e.ross@gmail.com>
2016-10-21 21:14:21 -0700

    branchone w solution to Problem Two testMultiply and update to Problem One w concantenation

--- lab-solution.js
+++ lab-solution.js
@@ -2,10 +2,21 @@
 
 function sum(a,b){
   var result = a + b;
-  console.log(result);
-  return [result , 'The sum of 4 and 7 is 11.'];
+  console.log(result , 'The sum of ' + a + ' and ' + b + ' is ' + result + '.');
+  return [result , 'The sum of ' + a + ' and ' + b + ' is ' + result + '.'];
 }
-
 sum(4, 7);
+
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
+////////////////////////////////////////////////////////////
+function multiply(a,b){ //eslint-disable-line
+  var productm = a * b;
+  console.log(productm , 'The product of ' + a + ' and ' + b + ' is ' + productm + '.');
+  return [productm , 'The product of ' + a + ' and ' + b + ' is ' + productm + '.'];
+}
+multiply(5 , 9);
+
+// Here is the test for multiply(); uncomment it to run it
+testMultiply(5,9);
+////////////////////////////////////////////////////////////
