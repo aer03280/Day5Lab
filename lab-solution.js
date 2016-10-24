de8239994b01197ed6bd3c19255491bd02516868
Ashley Ross <ashley.e.ross@gmail.com>
2016-10-23 19:12:57 -0700

    updating with code for problem 3

--- lab-solution.js
+++ lab-solution.js
@@ -1,42 +1,43 @@
-// 'use strict';
-//
-// function sum(a,b){
-//   var result = a + b;
-//   console.log(result , 'The sum of ' + a + ' and ' + b + ' is ' + result + '.');
-//   return [result , 'The sum of ' + a + ' and ' + b + ' is ' + result + '.'];
-// }
-// sum(4, 7);
-//
-// // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
-// ////////////////////////////////////////////////////////////
-// function multiply(a,b){ //eslint-disable-line
-//   var productm = a * b;
-//   console.log(productm , 'The product of ' + a + ' and ' + b + ' is ' + productm + '.');
-//   return [productm , 'The product of ' + a + ' and ' + b + ' is ' + productm + '.'];
-// }
-// multiply(5 , 9);
-//
-// // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
-// ////////////////////////////////////////////////////////////
+'use strict';
+////////////////////////////////////////////////////////////
+// PROBLEM 1
+function sum(a,b){
+  var result = a + b;
+  console.log(result , 'The sum of ' + a + ' and ' + b + ' is ' + result + '.');
+  return [result , 'The sum of ' + a + ' and ' + b + ' is ' + result + '.'];
+}
+sum(4, 7);
+
+// Here is the test for sum(); uncomment it to run it
+testSum(4, 7);
+////////////////////////////////////////////////////////////
+// PROBLEM 2
+function multiply(a,b){
+  var productm = a * b;
+  console.log(productm , 'The product of ' + a + ' and ' + b + ' is ' + productm + '.');
+  return [productm , 'The product of ' + a + ' and ' + b + ' is ' + productm + '.'];
+}
+multiply(5 , 9);
+
+// Here is the test for multiply(); uncomment it to run it
+testMultiply(5,9);
+////////////////////////////////////////////////////////////
+// PROBLEM 3
 function sum(a,b){
   var result = a + b;
   return result;
 }
-sum(4, 7);
 
-function multiply(a,b){ //eslint-disable-line
+function multiply(a,b){ 
   var productm = a * b;
   return productm;
 }
-multiply(5 , 9);
 
-function sumAndMultiply(a,b,c){//eslint-disable-line
+function sumAndMultiply(a,b,c){
   var sumOne = sum (a , b);
   var sumTwo = sum (sumOne , c);
-  var proOne = multiply (a,b);
-  var proTwo = multiply (proOne, c);
+  var proOne = multiply (a , b);
+  var proTwo = multiply (proOne , c);
 
   return [sumTwo , proTwo , a + ' and ' + b + ' and ' + c + ' sum to ' + sumTwo + '.' , 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + proTwo + '.'];
 }
@@ -44,3 +45,8 @@ function sumAndMultiply(a,b,c){//eslint-disable-line
 sumAndMultiply(4 ,7, 5);
 
 testSumAndMultiply(4,7,5);
+
+////////////////////////////////////////////////////////////
+// PROBLEM 4
+////////////////////////////////////////////////////////////
+// PROBELM 5
